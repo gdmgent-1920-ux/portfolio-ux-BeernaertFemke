93eb7db8914f8601a2e817a869cee99f4fa19439
BeernaertFemke <61620392+BeernaertFemke@users.noreply.github.com>
2020-03-21 16:12:07 +0100

    assigment 2
    assigment 2

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/b93645e8-d6f1-4c56-5b84-c4b3ccec220a-cf07/",
                 "target": "_blank"
             }
         ]
